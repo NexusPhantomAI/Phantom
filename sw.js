c091147de3c8fd22f6680c81ecfce79496bc61e1
NexusPhantomAI <schweineschach@yahoo.com>
2026-05-17 15:16:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
-const CACHE = 'phantom-v1';
-const FILES = [
-  './phantom-mode.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap'
-];
+const CACHE = 'phantom-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll([
+      './phantom-mode.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
   );
 });
 
@@ -15,12 +16,13 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./phantom-mode.html')))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
